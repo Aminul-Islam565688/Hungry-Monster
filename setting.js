8038714952f2db5edfe3e954a6ccb80edce5b66e
Aminul Islam <aminulislam565688@gmail.com>
2021-02-07 21:12:42 +0600

    last

--- setting.js
+++ setting.js
@@ -1,3 +1,4 @@
+// Random Food For At The beginning Function
 let foodList = document.getElementsByClassName("single-food-items");
 for (let i = 0; i < foodList.length; i++) {
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
@@ -6,16 +7,23 @@ for (let i = 0; i < foodList.length; i++) {
             let { meals } = data;
             let [mealList] = meals;
             let { idMeal, strMeal, strMealThumb } = mealList;
-            // console.log(idMeal, strMeal, strArea, strMealThumb);
+
+            // Food Image
             let foodImage = document.querySelectorAll('.food-image img');
             foodImage[i].src = strMealThumb;
+
+            // Food Name 
             let foodNameList = document.querySelectorAll("#singleFoodName");
             foodNameList[i].innerText = strMeal;
+
+            // Adding idMeal in Class
             foodImage[i].classList.add(idMeal);
             foodNameList[i].classList.add(idMeal);
         })
 }
 
+
+// Clicked And Show Food Ingredient Function
 let ingredient = (event) => {
     let targetFood = this.event.target;
     let foodId = targetFood.attributes[1].value;
@@ -25,16 +33,17 @@ let ingredient = (event) => {
             let { meals } = data;
             let [mealList] = meals;
             let { strMeal, strMealThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7 } = mealList;
-            console.log(strMeal, strMealThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7);
-            console.log(mealList);
+
             document.getElementById("foodItemsId").style.display = "none";
             document.getElementById("ingredientCart").style.display = "block";
             document.getElementById("ingredientImage").src = strMealThumb;
             document.getElementById("foodIngredientTitle").innerText = strMeal;
 
+            //Icon
             let icon = `
              <i class="fas fa-check-square food-icon"></i>
              `;
+            //Adding Icon and Ingredients
             document.getElementById("strIngredient1").innerHTML = icon + strIngredient1;
             document.getElementById("strIngredient2").innerHTML = icon + strIngredient2;
             document.getElementById("strIngredient3").innerHTML = icon + strIngredient3;
@@ -43,14 +52,65 @@ let ingredient = (event) => {
             document.getElementById("strIngredient6").innerHTML = icon + strIngredient6;
             document.getElementById("strIngredient7").innerHTML = icon + strIngredient7;
 
-            document.getElementById("foodSearchContent").style.display='none'
+            //Search Input and Button Display none
+            document.getElementById("foodSearchContent").style.display = 'none'
         })
 }
 
+
+// Main Function For Searched Food By User And It's Also Change Other's Food By Using Inputed Values Letter
 const foodSearchfunction = () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    })
+    let foodInput = document.getElementById("foodInput").value;
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + foodInput)
+        .then(res => res.json())
+        .then(data => {
+            let { meals } = data;
+            let [mealList] = meals;
+            let { idMeal, strMeal, strMealThumb } = mealList;
+
+            //Calling That Function That Will Give The Result Of Searched By User
+            searchFoodUpdate(0, idMeal, strMeal, strMealThumb);
+
+        })
+
+    // To Search Food By Letter Using substr(String Method)
+    let firstLetter = foodInput.substr(0, 1);
+    let secondLetter = foodInput.substr(1, 1);
+    let thirdLetter = foodInput.substr(2, 1);
+    let fourthLetter = foodInput.substr(3, 1);
+
+    searchFoodLetter(1, 2, firstLetter);
+    searchFoodLetter(3, 4, secondLetter);
+    searchFoodLetter(5, 6, thirdLetter);
+    searchFoodLetter(7, fourthLetter);
+}
+
+
+// Search Food by Letter Function
+let searchFoodLetter = (num1, num2 = 7, letter) => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=' + letter)
+        .then(res => res.json())
+        .then(data => {
+            let { meals } = data;
+            let [mealList1, mealList2] = meals;
+            searchFoodUpdate(num1, mealList1.idMeal, mealList1.strMeal, mealList1.strMealThumb);
+            searchFoodUpdate(num2, mealList2.idMeal, mealList2.strMeal, mealList2.strMealThumb);
+        })
+}
+
+
+// Searched Food Update and Ingredient Function
+let searchFoodUpdate = (i, idMeal, strMeal, strMealThumb) => {
+
+    let foodImage = document.querySelectorAll('.food-image img');
+    foodImage[i].src = strMealThumb;
+    let foodNameList = document.querySelectorAll("#singleFoodName");
+    foodNameList[i].innerText = strMeal;
+
+    let deleteFoodId = foodImage[i].attributes[1].value;
+
+    foodImage[i].classList.remove(deleteFoodId);
+    foodNameList[i].classList.remove(deleteFoodId);
+    foodImage[i].classList.add(idMeal);
+    foodNameList[i].classList.add(idMeal);
 }
